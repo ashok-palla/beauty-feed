48044059deb5ce9452f4a0ddc9b2d5e93364aa0c
root <ashok_palla@yahoo.com>
2019-11-30 00:36:18 +0530

    app change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,20 @@ export class HomeComponent implements OnInit {
     'assets/BackUp/12.jpg',
     'assets/BackUp/13.jpg',
     'assets/BackUp/18.jpg'];
+    public category = [
+      {
+        name: 'Tonique',
+        image_original_link: 'assets/GAIA_NATURELLE_1600x1600-300x300.jpg',
+        image_link: 'assets/GAIA_NATURELLE_1600x1600-300x300.jpg',
+        url: '#/product/1',
+      },
+      {
+        name: 'Cream',
+        image_original_link: 'assets/00001.jpg',
+        image_link: 'assets/00001.jpg',
+        url: '#/product/1',
+      }
+    ];
   public products = [
     {
       name: 'Collagen Shot',
@@ -24,6 +38,24 @@ export class HomeComponent implements OnInit {
       url: '#/product/1',
       mrp: 2990,
       offer_price: 50,
+      currency: 'EUR'
+    },
+    {
+      name: 'Hyaluron Face Cream',
+      image_original_link: 'assets/00001.jpg',
+      image_link: 'assets/00001.jpg',
+      url: '#/product/1',
+      mrp: 2440,
+      offer_price: 732,
+      currency: 'EUR'
+    },
+    {
+      name: 'ShotCa',
+      image_original_link: 'assets/00008.jpg',
+      image_link: 'assets/00008.jpg',
+      url: '#/product/1',
+      mrp: 2190,
+      offer_price: 657,
       currency: 'EUR'
     }
   ];
