e6ea94fa7e3a9b319e8463b2d9902661e374b9c5
root <ashok_palla@yahoo.com>
2019-11-30 13:36:46 +0530

    app change

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -9,4 +9,30 @@ export class SharedService {
         { name: 'Blog', link: 'blog' },
         { name: 'Contact', link: 'contact' },
     ];
+    public collegenshotbenefits = [
+        {
+            title: 'Fish collagen',
+            description: 'The small particle size allows for the best possible absorption and thus high product efficiency.',
+            image_url: 'assets/benefits/icon-riba.png',
+            height: 100
+        },
+        {
+            title: 'Vitamins and zinc',
+            description: 'For maximum efficiency, Vitamin C, E, B6 and biotin and zinc are added to Collagen shot.',
+            image_url: 'assets/benefits/icon-vitamini.png',
+            height: 100
+        },
+        {
+            title: 'MSM',
+            description: 'Organic sulfur involved in collagen formation. Because of its effects, it is often referred to as a mineral of beauty.',
+            image_url: 'assets/benefits/icon-msm.png',
+            height: 100
+        },
+        {
+            title: 'Easy dosing',
+            description: 'You take high levels of the active ingredients in one shot without mixing with other foods.',
+            image_url: 'assets/benefits/icon-doziranje.png',
+            height: 100
+        }
+    ];
 }
