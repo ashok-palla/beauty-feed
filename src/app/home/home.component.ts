671188caa5595474730828f68b9de36e7b92dc94
root <ashok_palla@yahoo.com>
2019-12-03 23:07:15 +0530

    app change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,60 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { SharedService } from '../shared.service';
+import { trigger, transition, style, query, animate, stagger, group, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  // animations: [
+  //   trigger('preview', [
+  //     transition(':enter', [
+  //       style({ overflow: 'hidden', height: 0 }),
+  //       query('.image-container', [
+  //         query('*', style({ opacity: 0 }))
+  //       ]),
+  //       group([
+  //         animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({ height: '*' })),
+  //         query('.image-container *', [
+  //           stagger(100, animate(500, style({ opacity: 1 })))
+  //         ])
+  //       ])
+  //     ]),
+  //     transition(':leave', [
+  //       style({ overflow: 'hidden' }),
+  //       animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({ height: '0px' }))
+  //     ]),
+  //     transition('* => *', [
+  //       query(':enter, :leave', style({ position: 'absolute', left: '0%' })),
+  //       query(':enter', style({ left: '100%' })),
+
+  //       group([
+  //         query(':leave', group([
+  //           animateChild(),
+  //           animate('1200ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 0, left: '-100%' }))
+  //         ])),
+
+  //         query(':enter', group([
+  //           animate('1200ms cubic-bezier(0.35, 0, 0.25, 1)', style('*')),
+  //           animateChild()
+  //         ]), { delay: 200 }),
+  //       ])
+  //     ])
+  //   ]),
+  //   trigger('image', [
+  //     transition(':enter', [
+  //       query('*', [
+  //         style({ transform: 'translateX(200px)', opacity: 0 }),
+  //         stagger(100, [
+  //           animate('1200ms cubic-bezier(0.35, 0, 0.25, 1)', style('*'))
+  //         ])
+  //       ])
+  //     ])
+  //   ])
+  // ]
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('@preview')
   timer;
   public pageIndex = 0;
   public imageArray = [
